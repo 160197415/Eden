1c65e084d0d901d16729926cc54e4a68f7c68d55
160197415 <160197415@aston.ac.uk>
2022-01-08 21:41:39 +0000

    actionlistener prototype

--- site/site/js/tableJS.js
+++ site/site/js/tableJS.js
@@ -56,3 +56,103 @@
                 tbody.appendChild(trow);
 
             }
+            var map;
+
+
+            /** Coordinates*/
+            const coordinates = { lat: 52.4862, lng: -1.8904 };
+            
+            
+            /** Initialising the map */
+            function initMap() {
+            
+                let mapOptions = {
+                    center: coordinates,
+                    zoom: 16
+                }
+            
+                let map = new google.maps.Map(document.getElementById('map'), mapOptions);
+            
+            
+                /**The marker will always start from a set coordinate */
+                let marker = new google.maps.Marker({
+                    position: coordinates,
+                    map: map,
+                    draggable: true,
+                    animation: google.maps.Animation.BOUNCE
+            
+            
+            
+                })
+            
+               
+            
+                const infoWiwndowOptionsContent = '<h5>When you drag me to a location click me to find the coordinates!</h5>';
+                const infoWindowOptions = {
+                    content: infoWiwndowOptionsContent,
+                    position: { lat: 52.4862, lng: -1.8904 },
+                    anchor: marker,
+                    maxWidth: 200,
+                    maxHeight: 20
+            
+            
+                }
+            
+                const infoWindow = new google.maps.InfoWindow(infoWindowOptions)
+            
+                marker.addListener('click', (googleMapsEvent) => {
+                   document.getElementsByClassName("position")[0].value =  ("Latitude:  " + googleMapsEvent.latLng.lat().toString() +  " Longitude: " + googleMapsEvent.latLng.lng().toString());
+            
+                })
+            
+                const infoWindowOpenOptions = {
+                    map: map,
+                    anchor: marker,
+                    shouldFocus: false,
+            
+            
+                }
+            
+                setTimeout(() => {
+                    infoWindow.setMap(null);
+                }, 5000);
+            
+            
+                infoWindow.open(infoWindowOpenOptions);
+            
+                infoWindow.addListener('closeclick', () => {
+                    infoWindow.close();
+                })
+                let newinfoWindow = new google.maps.InfoWindow();
+            
+                const locationButton = document.createElement("button");
+            
+                locationButton.textContent = "Pan to Current Location";
+                locationButton.classList.add("custom-map-control-button");
+                map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+                locationButton.addEventListener("click", () => {
+                    // Try HTML5 geolocation.
+                    if (navigator.geolocation) {
+                        navigator.geolocation.getCurrentPosition(
+                            (position) => {
+                                const pos = {
+                                    lat: position.coords.latitude,
+                                    lng: position.coords.longitude,
+                                };
+                                marker.setPosition(pos);
+                                newinfoWindow.setPosition(marker);
+            
+                                newinfoWindow.setContent("Location found. Click to find your Latitude and Longitudinal Position");
+                                newinfoWindow.open(map);
+                                map.setCenter(pos);
+                            },
+                            () => {
+                                handleLocationError(true, newinfoWindow, map.getCenter());
+                            }
+                        );
+                    } else {
+                        // Browser doesn't support Geolocation
+                        handleLocationError(false, newinfoWindow, map.getCenter());
+                    }
+                });
+            }
